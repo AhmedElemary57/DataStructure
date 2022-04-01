d42b26f7e3642fe42da7d258545d7127d3926178
Ahmedelsa3eed <73740339+Ahmedelsa3eed@users.noreply.github.com>
2022-04-01 15:06:51 +0200

    Delete lab3 directory

--- lab3/src/MaxHeapIF.java
+++
@@ -1,7 +0,0 @@
-public interface MaxHeapIF {
-    void maxHeapify(int[] A, int i);
-    void buildMaxHeap(int[] A);
-    void maxHeapInsert(int i);
-    int extractMax();
-    void heapSort(int[] A);
-}
